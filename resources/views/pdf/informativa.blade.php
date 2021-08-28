e12c10944ee6ed2b10b81cc17b0edff3ee43a3fc
coltrida <coltrida@gmail.com>
2021-08-28 12:37:15 +0200

    sistemata informativa privacy

--- resources/views/pdf/informativa.blade.php
+++ resources/views/pdf/informativa.blade.php
@@ -6,8 +6,8 @@
 
 <br><br>
 
-<div style="width: 100%; margin-top: 10px; margin-bottom: 30px; padding: 10px">
-    <h3>INFORMATIVA SUL TRATTAMENTO DEI DATI PERSONALI</h3>
+<div style="width: 100%; margin-top: 10px; margin-bottom: 30px; padding: 10px; font-size: 14px">
+    <h4>INFORMATIVA SUL TRATTAMENTO DEI DATI PERSONALI</h4>
     <p style="text-align: justify; text-justify: inter-word;">
         Ai sensi del D. Lgs n. 196/2003 (di seguito, per brevità, Codice Privacy) e del Regolamento UE
         n. 2016/679 (di seguito, per brevità, GDPR), il CENTRO UDITO S.R.L. con sede in Via M. Lalli n.10 - Pisa,
@@ -16,17 +16,17 @@
         <br><br>
         In conformtità alla normativa citata, all'interessato vengono date le seguenti informazioni:
     </p>
-    <h4 style="margin-bottom: 0">1. OGGETTO DEL TRATTAMENTO</h4>
+    <h5 style="margin-bottom: 0">1. OGGETTO DEL TRATTAMENTO</h5>
     <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
         Il trattamento ha ad oggetto dati personali identificativi dell'interessato nonchè dati relativi alla
         salute necessari per l'adempimento dei servizi e/o prestazioni richiesti.
     </p>
-    <h4 style="margin-bottom: 0">2. MODALITA' DEL TRATTAMENTO</h4>
+    <h5 style="margin-bottom: 0">2. MODALITA' DEL TRATTAMENTO</h5>
     <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
         I dati saranno raccolti manualmente e saranno trattati sia in modalità cartacea che in modalità elettronica
         e/o automatizzata atti a memorizzare, gestire e trasmettere i dati stessi.
     </p>
-    <h4 style="margin-bottom: 0">3. FINALITA' DEL TRATTAMENTO</h4>
+    <h5 style="margin-bottom: 0">3. FINALITA' DEL TRATTAMENTO</h5>
     <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
         I dati personali dell'interessato verranno richiesti per finalità specifiche.
     </p>
@@ -49,14 +49,72 @@
         Le basi giuridiche che legittimano il trattamento sono, pertanto, l'adempimento di un obbligo contrattuale,
         l'ottemperanza ad eventuali obblighi di legge e regolamenti ed il consenso dell'interessato.
     </p>
-</div>
-
-<div style="display:flex;">
-    <div style="font-size: 14px">
-        <div style="margin-left: 5px">Data: {{$provaSalvata->created_at->format('d-m-Y')}}</div>
-    </div>
-    <div style="float: right; font-size: 14px; margin-right: 40px">
-        <div>Firma</div>
-    </div>
+    <h5 style="margin-bottom: 0">4. NATURA DEL CONFERIMENTO DEI DATI E CONSEGUENTE RIFIUTO A RISPONDERE</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        Il conferimento dei dati per l'espletamento delle finalità di cui al punto 3.A è obbligatorio. <br><br>
+        In loro assenza il Titolare del Trattamento non potrà garantire la fornitura dei servizi richiesti dall'interessato. <br><br>
+        L'interessato potrà facoltativamente prestare il proprio consenso per le finalità di cui al punto 2.B o, comunque,
+        revocarlo in qualsiasi momento.
+    </p>
+    <h5 style="margin-bottom: 0">5. COMUNICAZIONE ED ACCESSO AI DATI</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        Il Titolare informa l'interessato che i suoi dati potranno essere comuncati , esclusivamente nel rispetto
+        delle finalità sopra descritte ai dipendenti ed ai collaboratori del Titolare, a società terze ed a professionisti
+         (a mero titolo esemplificativo: laboratori per l'esecuzione e la manutenzione degli apparecchi acustici, studi
+        professionali, consulenti, istituti di credito) che svolgono servizi per conto del Titolare. <br><br>
+        Inoltre i dati personali dell'interessato potrenno essere comunicati, senza alcuna ulteriore informativa e/o
+        consenso ad Organismi di Vigilanza, Autorità giudiziarie, a società di assicurazione per servizi assicurativi,
+        nonchè a tutti quei soggetti ai quali la comunicazione da parte del Titolare sia obbligatoria per legge.
+    </p>
+    <h5 style="margin-bottom: 0">6. DIFFUSIONE DEI DATI</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        I dati personali non sono soggetti a diffusione.
+    </p>
+    <h5 style="margin-bottom: 0">7. TRASFERIMENTO DEI DATI</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        I dati personali non sono soggetti a trasferimento all'estero.
+    </p>
+    <h5 style="margin-bottom: 0">8. PERIODO DI CONSERVAZIONE</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        I preventivi verranno conservati per un periodo non superiore a 3 (tre) mesi dalla consegna all'interessato,
+        dopodichè in assenza dell'ordine del Cliente verranno distrutti. <br><br>
+        In caso di ordine del Cliente, e comunque per tutte le altre finalità di cui al punto 3.A, i dati raccolti
+        verranno conservati per un periodo non superiore allo svolgimento dei servizi e/o prestazioni richieste nonchè, in
+        ambito contabile e fiscale, per il periodo richiesto dalla legge.<br><br>
+        Per quanto riguarda le finalità di cui al punto 3.B, i dati verranno utilizzati per un periodo non superiore
+        ai 24 (ventiquattro) mesi, dopodichè sarà necessario prestare nuovamente il proprio consenso.
+    </p>
+    <h5 style="margin-bottom: 0">9. DIRITTI DELL'INTERESSATO</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        L'interessato ha i diritti di cui agli artt. 15 e seguenti GDPR e precisamente i diritti di: <br><br>
+        - ottenere la conferma che sia o meno in corso un trattamento di dati personali che lo riguardano;
+        - ottenere l'accesso ai dati personali e alle informazioni relative al trattamento nonchè richiedere una copia
+        dei dati personali;
+        - ottenere la rettifica dei dati personali inesatti e l'integrazione dei dati personali incompleti;
+        - ottenere, qualora sussista una delle condizioni previste dall'art. 17 del GDPR, la cancellazione dei dati
+        personali che lo riguardano (diritto all'oblio);
+        - ottenere, nei casi previsti dall'art. 18 del GDPR, la limitazione del trattamento;
+        - ricevere i dati personali che lo riguardano in un formato strutturato, di uso comune e leggibile da dispositivo
+        automatico e richiedere la loro trasmissione ad un altro Titolare, se tecnicamente fattibile (diritto alla
+        portabilità dei dati);
+        - opposizione;
+        - proporre Reclamo all'Autorità Garante.
+    </p>
+    <h5 style="margin-bottom: 0">10. MODALITA' DI ESERCIZIO DEI DIRITTI</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        L'interessato potrà esercitare i propri diritti nelle forme e nei termini di cui all'art. 12 GDPR inviando una
+        raccomandata a/r a CENTRO UDITO S.R.L. VIA M. LALLI N. 10 - PISA oppure a mezzo e-mail all'indirizzo: <br>
+        info@centroudito.eu
+    </p>
+    <h5 style="margin-bottom: 0">11. TITOLARE DEL TRATTAMENTO</h5>
+    <p style="text-align: justify; text-justify: inter-word; margin-top: 0">
+        Il titolare del Trattamento è CENTRO UDITO S.R.L. <br><br>
+        Per presa visione <br><br>
+        Nome ________________________________ Cognome ___________________________________________ <br><br><br>
+        Luogo e data ____________________________________________ Firma _________________________________ <br><br><br>
+        Preso il consenso per le finalità di cui al punto 3.B (attività di profilazione per l'invio di comunicazioni).
+        Tale consenso potrà essere revocato successivamente in qualsiasi momento. <br><br><br>
+        Luogo e data ____________________________________________ Firma _________________________________
+    </p>
 </div>
 
